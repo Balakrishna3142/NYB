54d3b13816a20c061cb82b661f29f2ed8c055a87
Balakrishna <bala.krishna@nybinfotech.com>
2026-05-28 09:37:59 +0530

    commit initial

---
+++ JS/.primitive data types 27-05-2026/string/boolean/index.js
@@ -0,0 +1,30 @@
+  
+  var string = "hello"
+var K='hello'
+var _m =`hello`
+var balaKrishna ="hii"
+console.log(balaKrishna)
+console.log(K,typeof(K))
+console.log("vale of _m:",_m,typeof(_m))
+
+var M=90
+console.log(M)
+console.log(typeof(M))
+
+
+
+  
+  var a=true
+console.log(a)
+
+var b =false
+console.log(b)
+
+//undefiend
+var Ab
+console.log(Ab)
+//null
+
+var N = "Null"
+
+console.log(N)
---
+++ JS/.primitive data types 27-05-2026/string/index.js
@@ -0,0 +1,12 @@
+var string = "hello"
+var k ='hello'
+var m =`hello`
+
+console.log(k,typeof(k))
+console.log(m,typeof(m))
+console.log(string,typeof(string))
+
+
+
+//Boolean ((true/false)
+
