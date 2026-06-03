d8ac316aa964969fb2255df40da95728b20de2be
Balakrishna <bala.krishna@nybinfotech.com>
2026-06-03 15:20:53 +0530

    initial commit

---
+++ JS/condittionalstatements29-05-2026/index.js
@@ -0,0 +1,52 @@
+//nan 
+//0
+let age = 20;   
+if (age <= 18) {
+    console.log("you are eligible to vote");
+} else{
+    console.log("you are not eligible to vote");
+    
+
+}
+ 
+
+
+    
+//switch statement
+var  grade =9
+switch(grade){
+
+  case 10:
+        console.log("Excellent");
+        break;
+        
+        case 9:
+            console.log("good")
+            break;
+            
+            case 8:
+                console.log("average")
+}
+
+                //function add
+function grades(a){
+switch(g){
+
+  case 10:
+        console.log("Excellent");
+        break;
+        
+        case 9:
+            console.log("good")
+            break;
+            
+            case 8:
+                console.log("average")
+}
+}
+
+
+            
+
+
+
