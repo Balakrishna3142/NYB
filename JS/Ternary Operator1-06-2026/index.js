9a1fef6bc3c721c7cc42ea101f324c60365ca9e1
Balakrishna <bala.krishna@nybinfotech.com>
2026-06-03 15:22:32 +0530

    initial commit

---
+++ JS/Ternary Operator1-06-2026/index.js
@@ -0,0 +1,9 @@
+//age
+let age =45
+let category=(age<13)?"child":(age<20)?"teenager":(age<60)?"adult":"senior citizen";
+console.log(category);
+
+//login
+let isLoggedIn = true;
+let greeting = isLoggedIn ? "Welcome back!" : "Please log in.";
+console.log(greeting);
